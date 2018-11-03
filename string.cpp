73202f9b78a060cb540369b5d32f9dbffe997305
cthrvad <cthrvad@gmail.com>
2018-11-03 19:31:56 +0800

    add code

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s1="Hello World!";
+	string s2=s1;
+	string s3=s1+s2;
+	string s4;
+	cin>>s4;
+	cout<<s4<<endl;
+	cout<<s3<<endl;
+	return 0;
+}
