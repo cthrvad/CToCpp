8a678c866f6ac0cd55cd98b3435da6758cbb17c5
cthrvad <cthrvad@gmail.com>
2018-11-03 18:39:00 +0800

    add something

---
+++ bool.cpp
@@ -0,0 +1,16 @@
+//
+// Created by CTHRVAD on 2018/11/3.
+// This code is for CToC++
+// source: NO
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool flag=-1; // 这是真标志
+    bool flag1=0; // 这是假标志
+    cout<<flag<<endl;
+    cout<<flag1<<endl;
+    return 0;
+
+}
---
+++ cinandcout.cpp
@@ -0,0 +1,16 @@
+//
+// Created by CTHRVAD on 2018/11/3.
+// This code is for CToC++
+// source: NO
+//
+#include <cstdio> // 在这里不需要cstdio.h
+#include <iostream>
+using namespace std;
+int main()
+{
+    int s;
+    cin>>s;
+    cout<<s;
+    return 0;
+}
+
